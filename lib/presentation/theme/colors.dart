76ff5cf63000c9ec347a5e629b70959561a3ad7c
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-08-25 17:47:35 +0900

    💄 スタイルファイル追加

---
+++ lib/presentation/theme/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BrandColor {
+  static const base = Color.fromARGB(255, 247, 235, 225);
+  static const baseLight = Color.fromARGB(255, 255, 247, 241);
+  static const baseBlue = Color(0xffaad3d1);
+  static const baseRed = Color.fromARGB(255, 231, 161, 161);
+
+  static const white = Colors.white;
+  static const black = Color(0xff333333);
+}
+
+ThemeData brandThemeData = ThemeData(
+  scaffoldBackgroundColor: BrandColor.base,
+  colorScheme: const ColorScheme(
+    primary: BrandColor.baseBlue,
+    background: BrandColor.base,
+    brightness: Brightness.light,
+    error: Colors.red,
+    onBackground: BrandColor.baseBlue,
+    onError: Colors.transparent,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.black,
+    secondary: BrandColor.baseBlue,
+    surface: Colors.white,
+  ),
+);
