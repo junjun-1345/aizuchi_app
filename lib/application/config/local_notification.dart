49767271aaad2fc56b5b84c1ca23b5a792c4f206
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-10-25 14:21:29 +0900

    🎨 local notification create new file

---
+++ lib/application/config/local_notification.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class LocalNotificationService {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  void requestIOSPermission() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: false,
+          badge: true,
+          sound: false,
+        );
+  }
+
+  void initializePlatformSpecifics() {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: false,
+      onDidReceiveLocalNotification: (id, title, body, payload) async {
+        // your call back to the UI
+      },
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: (NotificationResponse res) {
+      debugPrint('payload:${res.payload}');
+    });
+  }
+
+  Future<void> showNotification() async {
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID',
+      'CHANNEL_NAME',
+      channelDescription: "CHANNEL_DESCRIPTION",
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: false,
+      timeoutAfter: 5000,
+      styleInformation: DefaultStyleInformation(true, true),
+    );
+
+    var iosChannelSpecifics = DarwinNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidChannelSpecifics, iOS: iosChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0, // Notification ID
+      'Test Title', // Notification Title
+      'Test Body', // Notification Body, set as null to remove the body
+      platformChannelSpecifics,
+      payload: 'New Payload', // Notification Payload
+    );
+  }
+
+  // ここから追加
+  Future<void> scheduleNotification() async {
+    // 5秒後
+    var scheduleNotificationDateTime = DateTime.now().add(Duration(seconds: 5));
+
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID 1',
+      'CHANNEL_NAME 1',
+      channelDescription: "CHANNEL_DESCRIPTION 1",
+      icon: 'app_icon',
+      //sound: RawResourceAndroidNotificationSound('my_sound'),
+      largeIcon: DrawableResourceAndroidBitmap('app_icon'),
+      enableLights: true,
+      color: const Color.fromARGB(255, 255, 0, 0),
+      ledColor: const Color.fromARGB(255, 255, 0, 0),
+      ledOnMs: 1000,
+      ledOffMs: 500,
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: false,
+      timeoutAfter: 5000,
+      styleInformation: DefaultStyleInformation(true, true),
+    );
+
+    var iosChannelSpecifics = DarwinNotificationDetails(
+        //sound: 'my_sound.aiff',
+        );
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidChannelSpecifics,
+      iOS: iosChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Test Title',
+      'Test Body',
+      tz.TZDateTime.from(scheduleNotificationDateTime, tz.local), // 5秒後に表示
+      platformChannelSpecifics,
+      payload: 'Test Payload',
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+}
--- lib/presentation/widgets/humburger_menu_widget.dart
+++ lib/presentation/widgets/humburger_menu_widget.dart
@@ -1,3 +1,4 @@
+import 'package:aizuchi_app/application/config/local_notification.dart';
 import 'package:aizuchi_app/infrastructure/firebase/auth_imp.dart';
 import 'package:aizuchi_app/presentation/router/router.dart';
 import 'package:aizuchi_app/presentation/widgets/button_widget.dart';
@@ -6,8 +7,6 @@ import 'package:go_router/go_router.dart';
 
 import '../theme/colors.dart';
 
-import 'package:timezone/data/latest_all.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class HumburgerMenu extends StatelessWidget {
@@ -15,141 +14,6 @@ class HumburgerMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-    // 1回目に通知を飛ばす時間の作成
-    tz.TZDateTime _nextInstanceOf8AM() {
-      final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-      tz.TZDateTime scheduledDate = tz.TZDateTime(
-          tz.local, now.year, now.month, now.day, now.hour, now.minute, 30);
-      if (scheduledDate.isBefore(now)) {
-        scheduledDate = scheduledDate.add(const Duration(days: 1));
-      }
-      return scheduledDate;
-    }
-
-    void _initializePlatformSpecifics() {
-      var initializationSettingsAndroid =
-          AndroidInitializationSettings('app_icon');
-
-      var initializationSettingsIOS = DarwinInitializationSettings(
-        requestAlertPermission: true,
-        requestBadgePermission: true,
-        requestSoundPermission: false,
-        onDidReceiveLocalNotification: (id, title, body, payload) async {
-          // your call back to the UI
-        },
-      );
-
-      var initializationSettings = InitializationSettings(
-          android: initializationSettingsAndroid,
-          iOS: initializationSettingsIOS);
-
-      flutterLocalNotificationsPlugin.initialize(initializationSettings,
-          onDidReceiveNotificationResponse: (NotificationResponse res) {
-        debugPrint('payload:${res.payload}');
-      });
-    }
-
-    Future<void> _showNotification() async {
-      var androidChannelSpecifics = AndroidNotificationDetails(
-        'CHANNEL_ID',
-        'CHANNEL_NAME',
-        channelDescription: "CHANNEL_DESCRIPTION",
-        importance: Importance.max,
-        priority: Priority.high,
-        playSound: false,
-        timeoutAfter: 5000,
-        styleInformation: DefaultStyleInformation(true, true),
-      );
-
-      var iosChannelSpecifics = DarwinNotificationDetails();
-
-      var platformChannelSpecifics = NotificationDetails(
-          android: androidChannelSpecifics, iOS: iosChannelSpecifics);
-
-      await flutterLocalNotificationsPlugin.show(
-        0, // Notification ID
-        'Test Title', // Notification Title
-        'Test Body', // Notification Body, set as null to remove the body
-        platformChannelSpecifics,
-        payload: 'New Payload', // Notification Payload
-      );
-    }
-
-    Future<void> _scheduleDaily8AMNotification() async {
-      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-          FlutterLocalNotificationsPlugin();
-
-      await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        'OB-1',
-        '本日の顔を撮影をしましょう',
-        _nextInstanceOf8AM(),
-        const NotificationDetails(
-          android: AndroidNotificationDetails(
-            'ob-1-face-daily',
-            'ob-1-face-daily',
-            channelDescription: 'Face photo notification',
-          ),
-          iOS: DarwinNotificationDetails(
-            badgeNumber: 1,
-          ),
-        ),
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
-        matchDateTimeComponents: DateTimeComponents.time,
-        androidAllowWhileIdle: true,
-      );
-    }
-
-    // ここから追加
-    Future<void> _scheduleNotification() async {
-      // 5秒後
-      var scheduleNotificationDateTime =
-          DateTime.now().add(Duration(seconds: 5));
-
-      var androidChannelSpecifics = AndroidNotificationDetails(
-        'CHANNEL_ID 1',
-        'CHANNEL_NAME 1',
-        channelDescription: "CHANNEL_DESCRIPTION 1",
-        icon: 'app_icon',
-        //sound: RawResourceAndroidNotificationSound('my_sound'),
-        largeIcon: DrawableResourceAndroidBitmap('app_icon'),
-        enableLights: true,
-        color: const Color.fromARGB(255, 255, 0, 0),
-        ledColor: const Color.fromARGB(255, 255, 0, 0),
-        ledOnMs: 1000,
-        ledOffMs: 500,
-        importance: Importance.max,
-        priority: Priority.high,
-        playSound: false,
-        timeoutAfter: 5000,
-        styleInformation: DefaultStyleInformation(true, true),
-      );
-
-      var iosChannelSpecifics = DarwinNotificationDetails(
-          //sound: 'my_sound.aiff',
-          );
-
-      var platformChannelSpecifics = NotificationDetails(
-        android: androidChannelSpecifics,
-        iOS: iosChannelSpecifics,
-      );
-
-      await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        'Test Title',
-        'Test Body',
-        tz.TZDateTime.from(scheduleNotificationDateTime, tz.local), // 5秒後に表示
-        platformChannelSpecifics,
-        payload: 'Test Payload',
-        androidAllowWhileIdle: true,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
-      );
-    }
-
     final pop = Container(
       child: Column(children: [
         Container(
@@ -292,9 +156,7 @@ class HumburgerMenu extends StatelessWidget {
             button,
             ElevatedButton(
               onPressed: () async {
-                _initializePlatformSpecifics();
-                _showNotification();
-                _scheduleDaily8AMNotification();
+                LocalNotificationService().showNotification();
               },
               child: const Text("今すぐ通知"),
             ),
