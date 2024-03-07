97686f34062a1c9781cd219c225ce5de6a177e5c
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-03-07 16:50:10 +0900

    logPageの作成

--- lib/presentation/view/pages/log/log_page.dart
+++ lib/presentation/view/pages/log/log_page.dart
@@ -1,6 +1,7 @@
 import 'package:aizuchi_app/domain/entity/enums/emotion.dart';
 import 'package:aizuchi_app/presentation/router/router.dart';
-import 'package:aizuchi_app/presentation/state/daily_state.dart';
+import 'package:aizuchi_app/presentation/view/pages/log/components/log_carousel.dart';
+import 'package:aizuchi_app/presentation/view/pages/log/components/log_summary_tile.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,49 +20,20 @@ class LogPage extends ConsumerWidget {
       // print(authenticated);
     }
 
-    return Center(
-      child: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 80),
-            const Text('MessagePage'),
-            Expanded(
-              child: ref.watch(dailyNotifierProvider).when(
-                    data: (dailies) => ListView.builder(
-                      // reverse: true,
-                      itemCount: dailies.length,
-                      itemBuilder: (context, index) {
-                        final daily = dailies[index];
-                        return Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(daily.createdAt.toString()),
-                                const SizedBox(width: 8),
-                                Text(
-                                  daily.emotion.emotionValue ?? '',
-                                  style: const TextStyle(fontSize: 24),
-                                ),
-                              ],
-                            ),
-                            Text(daily.summary),
-                          ],
-                        );
-                      },
-                    ),
-                    loading: () =>
-                        const Center(child: CircularProgressIndicator()),
-                    error: (error, _) =>
-                        Center(child: Text('エラーが発生しました: $error')),
-                  ),
-            ),
-          ],
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('ログ'),
+        LogSummaryTile(),
+        SizedBox(
+          height: 24,
         ),
-      ),
+        LogCarousel(),
+        SizedBox(
+          height: 24,
+        ),
+      
+      ],
     );
   }
 }
