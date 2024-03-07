4c586600b4ad1039cd61a3069e1ae15605312132
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-03-07 16:50:10 +0900

    logPageの作成

--- lib/presentation/view/pages/log/log_page.dart
+++ lib/presentation/view/pages/log/log_page.dart
@@ -1,4 +1,6 @@
-import 'package:aizuchi_app/presentation/state/summary_state.dart';
+import 'package:aizuchi_app/presentation/router/router.dart';
+import 'package:aizuchi_app/presentation/view/pages/log/components/log_carousel.dart';
+import 'package:aizuchi_app/presentation/view/pages/log/components/log_summary_tile.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
@@ -19,75 +21,20 @@ class LogPage extends HookConsumerWidget {
           .getWeeklySummary(selectDate.value);
     }
 
-    return Center(
-      child: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 160),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  DatePicker.showDatePicker(context,
-                      showTitleActions: true,
-                      minTime: DateTime(1900, 1, 1),
-                      maxTime: DateTime(2100, 1, 1), onConfirm: (date) {
-                    selectDate.value = date;
-                  }, currentTime: DateTime.now(), locale: LocaleType.jp);
-                },
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  selectDate.value == DateTime(0)
-                      ? "指定日を入力"
-                      : "${selectDate.value.year}年 ${selectDate.value.month}月 ${selectDate.value.day}日",
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                get();
-              },
-              child: const Text("検索"),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ref.watch(summaryNotifierProvider).when(
-                    data: (dailies) => ListView.builder(
-                      // reverse: true,
-                      itemCount: dailies.length,
-                      itemBuilder: (context, index) {
-                        final daily = dailies[index];
-                        return Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(daily.createdAt.toString()),
-                                const SizedBox(width: 8),
-                              ],
-                            ),
-                            Text(daily.content),
-                          ],
-                        );
-                      },
-                    ),
-                    loading: () =>
-                        const Center(child: CircularProgressIndicator()),
-                    error: (error, _) =>
-                        Center(child: Text('エラーが発生しました: $error')),
-                  ),
-            ),
-          ],
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('ログ'),
+        LogSummaryTile(),
+        SizedBox(
+          height: 24,
         ),
-      ),
+        LogCarousel(),
+        SizedBox(
+          height: 24,
+        ),
+      
+      ],
     );
   }
 }
