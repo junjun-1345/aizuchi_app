5d612fdb2a96b328e74e61db713cf81e3d6022b7
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-03-10 22:58:13 +0900

    VMの導入

--- lib/presentation/view/pages/log/components/select_week.dart
+++ lib/presentation/view/pages/log/components/select_week.dart
@@ -1,18 +1,29 @@
+import 'package:aizuchi_app/presentation/view_model/log_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
-class SelectWeekPart extends StatelessWidget {
+class SelectWeekPart extends ConsumerWidget {
   const SelectWeekPart({
+    required this.logStartDay,
     super.key,
   });
 
+  final DateTime logStartDay;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String startDay = DateFormat('M/d').format(logStartDay);
+    final String endDay =
+        DateFormat('M/d').format(logStartDay.add(const Duration(days: 6)));
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            ref.read(logViewModelProvider).changeToNextWeek();
+          },
           child: Container(
             height: 25,
             width: 25,
@@ -32,9 +43,9 @@ class SelectWeekPart extends StatelessWidget {
         const SizedBox(
           width: 8,
         ),
-        const Text(
-          '1/13(水) 〜 1/19(火)',
-          style: TextStyle(
+        Text(
+          '$startDay 〜 $endDay',
+          style: const TextStyle(
             fontSize: 16,
             color: Color(0xffe79f95),
           ),
@@ -43,7 +54,9 @@ class SelectWeekPart extends StatelessWidget {
           width: 8,
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            ref.read(logViewModelProvider).changeToNextWeek();
+          },
           child: Container(
             height: 25,
             width: 25,
