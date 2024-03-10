0fd9942b578ef724826108e55a6332a891d855ad
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-03-10 17:04:30 +0900

    プリントの削除

--- lib/infrastructure/repositories/daily_db_repositories_impl.dart
+++ lib/infrastructure/repositories/daily_db_repositories_impl.dart
@@ -8,7 +8,7 @@ class DailyDBRepositoryImpl implements DailyDBRepository {
   @override
   Future<List<DailyEntity>> read(DateTime startDate, DateTime endDate) async {
     final id = FirebaseAuth.instance.currentUser?.uid ?? '';
-
+¥
     try {
       final List<DailyEntity> dailies = [];
       await FirebaseFirestore.instance
