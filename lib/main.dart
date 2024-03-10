0b7fab2c8e145497c88d7e1be3162709c2baf2c4
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-03-10 22:57:26 +0900

    ロケールデータの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:aizuchi_app/presentation/router/router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest_all.dart' as tz;
@@ -13,6 +14,7 @@ import 'package:timezone/data/latest_all.dart' as tz;
 ///
 
 void main() async {
+  await initializeDateFormatting();
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
   tz.setLocalLocation(tz.getLocation("Asia/Tokyo"));
