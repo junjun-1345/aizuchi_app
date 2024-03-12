189f6b320f8e3eceb43e4fa7588783a27a7a4967
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-03-12 18:05:13 +0900

    summary state追加

---
+++ lib/presentation/state/summary_state.dart
@@ -0,0 +1,45 @@
+import 'package:aizuchi_app/domain/domain_module.dart';
+import 'package:aizuchi_app/domain/entity/models/daily.dart';
+import 'package:aizuchi_app/domain/usecases/daily_usecase.dart';
+import 'package:aizuchi_app/mock/daily_mock.dart';
+import 'package:aizuchi_app/presentation/model/summary_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final summaryNotifierProvider =
+    StateNotifierProvider<SummaryNotifier, AsyncValue<List<SummaryModel>>>(
+  (ref) {
+    return SummaryNotifier(
+      ref,
+      //本番
+      // ref.watch(dailyUsecaseProvider);
+      //mock
+      FakeDailyUsecases(),
+    );
+  },
+);
+
+class SummaryNotifier extends StateNotifier<AsyncValue<List<SummaryModel>>> {
+  SummaryNotifier(this.ref, this._dailyUsecase)
+      : super(const AsyncValue.loading()) {
+    initialize();
+  }
+
+  final Ref ref;
+  final DailyUsecases _dailyUsecase;
+
+  void initialize() async {
+    final List<DailyEntity> dailiesEntity = await _dailyUsecase.readWeek();
+    final List<SummaryModel> summaries =
+        dailiesEntity.map((daily) => SummaryModel.fromEntity(daily)).toList();
+    print("初期化$summaries");
+    state = AsyncValue.data(summaries);
+  }
+
+  Future<void> getWeeklySummary(DateTime endDate) async {
+    final List<DailyEntity> dailiesEntity =
+        await _dailyUsecase.readWeek(endDate: endDate);
+    final List<SummaryModel> summaries =
+        dailiesEntity.map((daily) => SummaryModel.fromEntity(daily)).toList();
+    state = AsyncValue.data(summaries);
+  }
+}
--- lib/presentation/view/pages/log/log_page.dart
+++ lib/presentation/view/pages/log/log_page.dart
@@ -1,35 +1,65 @@
-import 'package:aizuchi_app/domain/entity/enums/emotion.dart';
-import 'package:aizuchi_app/presentation/router/router.dart';
-import 'package:aizuchi_app/presentation/state/daily_state.dart';
+import 'package:aizuchi_app/presentation/state/summary_state.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
-class LogPage extends ConsumerWidget {
+class LogPage extends HookConsumerWidget {
   const LogPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Future<void> signOut() async {
-      await FirebaseAuth.instance.signOut();
-      context.router.replace(const SignInRoute());
-      // final authenticated = FirebaseAuth.instance.currentUser != null;
-      // print(authenticated);
+    final selectDate = useState(DateTime(0));
+
+    Future<void> get() async {
+      ref
+          .read(summaryNotifierProvider.notifier)
+          .getWeeklySummary(selectDate.value);
     }
 
     return Center(
       child: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 80),
-            const Text('MessagePage'),
+            const SizedBox(height: 160),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  DatePicker.showDatePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(1900, 1, 1),
+                      maxTime: DateTime(2100, 1, 1), onConfirm: (date) {
+                    selectDate.value = date;
+                  }, currentTime: DateTime.now(), locale: LocaleType.jp);
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  selectDate.value == DateTime(0)
+                      ? "指定日を入力"
+                      : "${selectDate.value.year}年 ${selectDate.value.month}月 ${selectDate.value.day}日",
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                get();
+              },
+              child: const Text("検索"),
+            ),
+            const SizedBox(height: 20),
             Expanded(
-              child: ref.watch(dailyNotifierProvider).when(
+              child: ref.watch(summaryNotifierProvider).when(
                     data: (dailies) => ListView.builder(
                       // reverse: true,
                       itemCount: dailies.length,
@@ -42,13 +72,9 @@ class LogPage extends ConsumerWidget {
                               children: [
                                 Text(daily.createdAt.toString()),
                                 const SizedBox(width: 8),
-                                Text(
-                                  daily.emotion.emotionValue ?? '',
-                                  style: const TextStyle(fontSize: 24),
-                                ),
                               ],
                             ),
-                            Text(daily.summary),
+                            Text(daily.content),
                           ],
                         );
                       },
