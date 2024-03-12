527abbfd5644ddd659343a864ecb5ce85bcecd22
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-03-12 18:05:13 +0900

    summary state追加

--- lib/domain/repositories/daily_db_repository.dart
+++ lib/domain/repositories/daily_db_repository.dart
@@ -1,11 +1,9 @@
 import 'package:aizuchi_app/domain/entity/models/daily.dart';
 
-
 abstract class DailyDBRepository {
   Future<void> create(DailyEntity daily);
   Future<void> updateLatest(DailyEntity daily);
 
   Future<List<DailyEntity>> readAll();
   Future<List<DailyEntity>> read(DateTime startDate, DateTime endDate);
-
 }
