2b587d68fcb3ab7359810e963dd587d718f08d52
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-03-17 02:32:38 +0900

    日付変更

--- lib/presentation/view/pages/log/components/select_week.dart
+++ lib/presentation/view/pages/log/components/select_week.dart
@@ -1,28 +1,33 @@
-import 'package:aizuchi_app/presentation/view_model/log_view_model.dart';
+import 'package:aizuchi_app/presentation/state/summary_state.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class SelectWeekPart extends ConsumerWidget {
+class SelectWeekPart extends HookConsumerWidget {
   const SelectWeekPart({
-    required this.logStartDay,
     super.key,
   });
 
-  final DateTime logStartDay;
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final String startDay = DateFormat('M/d').format(logStartDay);
-    final String endDay =
-        DateFormat('M/d').format(logStartDay.add(const Duration(days: 6)));
+    final logEndDate = useState(DateTime.now());
+    final logStartDate =
+        useState(DateTime.now().subtract(const Duration(days: 6)));
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         GestureDetector(
           onTap: () {
-            ref.read(logViewModelProvider).changeToPrevWeek();
+            logEndDate.value =
+                logEndDate.value.subtract(const Duration(days: 1));
+            logStartDate.value =
+                logStartDate.value.subtract(const Duration(days: 1));
+            ref
+                .read(summaryNotifierProvider.notifier)
+                .getWeeklySummary(logEndDate.value);
           },
           child: Container(
             height: 25,
@@ -44,7 +49,7 @@ class SelectWeekPart extends ConsumerWidget {
           width: 8,
         ),
         Text(
-          '$startDay 〜 $endDay',
+          '${logStartDate.value.month}/${logStartDate.value.day}(${DateFormat.EEEE('ja').format(logStartDate.value)[0]})〜${logEndDate.value.month}/${logEndDate.value.day}(${DateFormat.EEEE('ja').format(logEndDate.value)[0]})',
           style: const TextStyle(
             fontSize: 16,
             color: Color(0xffe79f95),
@@ -55,7 +60,12 @@ class SelectWeekPart extends ConsumerWidget {
         ),
         GestureDetector(
           onTap: () {
-            ref.read(logViewModelProvider).changeToNextWeek();
+            logEndDate.value = logEndDate.value.add(const Duration(days: 1));
+            logStartDate.value =
+                logStartDate.value.add(const Duration(days: 1));
+            ref
+                .read(summaryNotifierProvider.notifier)
+                .getWeeklySummary(logEndDate.value);
           },
           child: Container(
             height: 25,
