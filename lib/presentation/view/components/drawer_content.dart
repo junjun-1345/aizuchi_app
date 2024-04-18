103e08ea0cadd682b3692953835b66704924530d
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-04-18 14:47:12 +0900

    リファクタリング

--- lib/presentation/view/components/drawer_content.dart
+++ lib/presentation/view/components/drawer_content.dart
@@ -3,7 +3,6 @@ import 'package:aizuchi_app/domain/entity/models/color.dart';
 import 'package:aizuchi_app/presentation/router/router.dart';
 import 'package:aizuchi_app/presentation/state/user_state.dart';
 import 'package:aizuchi_app/presentation/view/components/attention_dialog.dart';
-import 'package:aizuchi_app/presentation/view_model/subscription_view_model.dart';
 import 'package:aizuchi_app/presentation/view_model/users_view_model.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -23,17 +22,21 @@ class HamburgerMenu extends ConsumerWidget {
           padding: const EdgeInsets.fromLTRB(16, 56, 16, 24),
           child: Column(
             children: <Widget>[
-              userState.when(data: (data) {
-                if (!data.isSubscription) {
-                  return _buildPromoSection();
-                } else {
-                  return const SizedBox();
-                }
-              }, loading: () {
-                return const CircularProgressIndicator();
-              }, error: (error, stack) {
-                return const Text('エラーが発生しました。');
-              }),
+              userState.when(
+                data: (data) {
+                  if (!data.isSubscription) {
+                    return _buildPromoSection();
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+                loading: () {
+                  return const CircularProgressIndicator();
+                },
+                error: (error, stack) {
+                  return const Text('エラーが発生しました。');
+                },
+              ),
               const SizedBox(height: 24),
               _buildFeatureSection(context),
               const SizedBox(height: 24),
