a65366c10bb493a73fdab5405288cafa64149787
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-04-24 16:10:04 +0900

    無駄なコードの修正

--- lib/presentation/view/pages/search/search.dart
+++ lib/presentation/view/pages/search/search.dart
@@ -1,12 +1,7 @@
-import 'package:aizuchi_app/domain/entity/enums/shared_preferences_key.dart';
 import 'package:aizuchi_app/domain/entity/models/color.dart';
-import 'package:aizuchi_app/infrastructure/data_model.dart';
-import 'package:aizuchi_app/presentation/view/components/app_button.dart';
 import 'package:aizuchi_app/presentation/view/components/drawer_content.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
