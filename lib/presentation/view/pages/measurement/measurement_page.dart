753f0a76d466bd0b8d48a23b5eb8ded6a968780a
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-06-01 19:10:26 +0900

    出典の追加

--- lib/presentation/view/pages/measurement/measurement_page.dart
+++ lib/presentation/view/pages/measurement/measurement_page.dart
@@ -17,46 +17,52 @@ class MeasurementPage extends HookConsumerWidget {
       appBar: const AppAppBar(
         title: "こころの計測",
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              _menuContent(
-                title: "憂うつレベル(PHQ-9)",
-                description: "９個の質問で、「憂うつレベル」を測定します。",
-                imgUrl: "mofumofu_icon_1.png",
-                onPreviousResults: null,
-                onStart: null,
-                context: context,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              _menuContent(
-                title: "ストレスチェック",
-                description: "30個の質問で「ストレスレベル」を測定します。",
-                imgUrl: "mofumofu_icon_2.png",
-                onPreviousResults: null,
-                onStart: null,
-                context: context,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              _menuContent(
-                title: "睡眠レベル(アテネ不眠尺度)",
-                description: "8個の質問で「睡眠レベル」を測定します。",
-                imgUrl: "mofumofu_icon_3.png",
-                onPreviousResults: null,
-                onStart: null,
-                context: context,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              const Text("アップデートにて順次公開予定です。")
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                _menuContent(
+                  title: "憂うつレベル(PHQ-9)",
+                  description: "９個の質問で、「憂うつレベル」を測定します。",
+                  imgUrl: "mofumofu_icon_1.png",
+                  source: "Patient Health Questionnaire（PHQ-9）こころとからだの質問票",
+                  onPreviousResults: null,
+                  onStart: null,
+                  context: context,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                _menuContent(
+                  title: "ストレスチェック",
+                  description: "30個の質問で「ストレスレベル」を測定します。",
+                  imgUrl: "mofumofu_icon_2.png",
+                  source: "簡易ストレス度チェックリスト（日大桂・村上版）",
+                  onPreviousResults: null,
+                  onStart: null,
+                  context: context,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                _menuContent(
+                  title: "睡眠レベル(アテネ不眠尺度)",
+                  description: "8個の質問で「睡眠レベル」を測定します。",
+                  imgUrl: "mofumofu_icon_3.png",
+                  source:
+                      "アテネ不眠尺度日本語版（Japanese version of the Athens Insomnia Scale: AIS-J）",
+                  onPreviousResults: null,
+                  onStart: null,
+                  context: context,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text("アップデートにて順次公開予定です。")
+              ],
+            ),
           ),
         ),
       ),
@@ -67,6 +73,7 @@ class MeasurementPage extends HookConsumerWidget {
     required String title,
     required String description,
     required String imgUrl,
+    required String source,
     required void Function()? onPreviousResults,
     required void Function()? onStart,
     required BuildContext context,
@@ -76,9 +83,9 @@ class MeasurementPage extends HookConsumerWidget {
         color: BrandColor.white,
         borderRadius: BorderRadius.circular(8),
       ),
-      height: 140,
+      height: 184,
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -138,6 +145,12 @@ class MeasurementPage extends HookConsumerWidget {
                 height: MediaQuery.of(context).size.height * 0.08,
               ),
             ],
+          ),
+          Text(
+            "※出典 : $source",
+            style: TextStyle(
+              color: BrandColor.black.withOpacity(0.5),
+            ),
           ),
         ],
       ),
