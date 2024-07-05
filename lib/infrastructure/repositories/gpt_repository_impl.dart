7eaa557cb20d25d4aae798fe7589b6ab6e1d5888
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-07-05 13:49:00 +0900

    test

--- lib/infrastructure/repositories/gpt_repository_impl.dart
+++ lib/infrastructure/repositories/gpt_repository_impl.dart
@@ -15,6 +15,8 @@ class GptRepositoryImpl implements GptRepository {
   static const String _url = 'https://api.openai.com/v1/chat/completions';
   static final _key = Env.gptKey;
 
+  
+
   @override
   Future<String> createSummary(
     List<MessageEntity> messagesEntity,
