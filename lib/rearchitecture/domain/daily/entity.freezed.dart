bfbd075a1815afea1a9e1b53d9817d432055d809
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-07-17 21:07:36 +0900

    daily entity 作成

---
+++ lib/rearchitecture/domain/daily/entity.freezed.dart
@@ -0,0 +1,193 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'entity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Daily _$DailyFromJson(Map<String, dynamic> json) {
+  return _Daily.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Daily {
+  String? get emotion => throw _privateConstructorUsedError;
+  String? get summary => throw _privateConstructorUsedError;
+  @TimestampConverter()
+  DateTime? get createdAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $DailyCopyWith<Daily> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DailyCopyWith<$Res> {
+  factory $DailyCopyWith(Daily value, $Res Function(Daily) then) =
+      _$DailyCopyWithImpl<$Res, Daily>;
+  @useResult
+  $Res call(
+      {String? emotion,
+      String? summary,
+      @TimestampConverter() DateTime? createdAt});
+}
+
+/// @nodoc
+class _$DailyCopyWithImpl<$Res, $Val extends Daily>
+    implements $DailyCopyWith<$Res> {
+  _$DailyCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? emotion = freezed,
+    Object? summary = freezed,
+    Object? createdAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      emotion: freezed == emotion
+          ? _value.emotion
+          : emotion // ignore: cast_nullable_to_non_nullable
+              as String?,
+      summary: freezed == summary
+          ? _value.summary
+          : summary // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createdAt: freezed == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$DailyImplCopyWith<$Res> implements $DailyCopyWith<$Res> {
+  factory _$$DailyImplCopyWith(
+          _$DailyImpl value, $Res Function(_$DailyImpl) then) =
+      __$$DailyImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? emotion,
+      String? summary,
+      @TimestampConverter() DateTime? createdAt});
+}
+
+/// @nodoc
+class __$$DailyImplCopyWithImpl<$Res>
+    extends _$DailyCopyWithImpl<$Res, _$DailyImpl>
+    implements _$$DailyImplCopyWith<$Res> {
+  __$$DailyImplCopyWithImpl(
+      _$DailyImpl _value, $Res Function(_$DailyImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? emotion = freezed,
+    Object? summary = freezed,
+    Object? createdAt = freezed,
+  }) {
+    return _then(_$DailyImpl(
+      emotion: freezed == emotion
+          ? _value.emotion
+          : emotion // ignore: cast_nullable_to_non_nullable
+              as String?,
+      summary: freezed == summary
+          ? _value.summary
+          : summary // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createdAt: freezed == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$DailyImpl implements _Daily {
+  const _$DailyImpl(
+      {this.emotion, this.summary, @TimestampConverter() this.createdAt});
+
+  factory _$DailyImpl.fromJson(Map<String, dynamic> json) =>
+      _$$DailyImplFromJson(json);
+
+  @override
+  final String? emotion;
+  @override
+  final String? summary;
+  @override
+  @TimestampConverter()
+  final DateTime? createdAt;
+
+  @override
+  String toString() {
+    return 'Daily(emotion: $emotion, summary: $summary, createdAt: $createdAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DailyImpl &&
+            (identical(other.emotion, emotion) || other.emotion == emotion) &&
+            (identical(other.summary, summary) || other.summary == summary) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, emotion, summary, createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DailyImplCopyWith<_$DailyImpl> get copyWith =>
+      __$$DailyImplCopyWithImpl<_$DailyImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$DailyImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Daily implements Daily {
+  const factory _Daily(
+      {final String? emotion,
+      final String? summary,
+      @TimestampConverter() final DateTime? createdAt}) = _$DailyImpl;
+
+  factory _Daily.fromJson(Map<String, dynamic> json) = _$DailyImpl.fromJson;
+
+  @override
+  String? get emotion;
+  @override
+  String? get summary;
+  @override
+  @TimestampConverter()
+  DateTime? get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$DailyImplCopyWith<_$DailyImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
