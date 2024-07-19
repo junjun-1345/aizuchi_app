ff5bb1e788c9fc80361ba720f8e3ea73f635bade
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-07-19 18:24:19 +0900

    daily repo 作成

---
+++ lib/rearchitecture/repo/daily/repository.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dailyRepoHash() => r'69c144b8c82cd7b28161cf960f74adad0c891f7d';
+
+/// See also [DailyRepo].
+@ProviderFor(DailyRepo)
+final dailyRepoProvider = NotifierProvider<DailyRepo, void>.internal(
+  DailyRepo.new,
+  name: r'dailyRepoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$dailyRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DailyRepo = Notifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
